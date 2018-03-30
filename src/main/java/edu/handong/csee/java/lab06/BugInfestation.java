c5f4921165f97f086932fea09a5a4129e036a08b
21700383 <21700383@handong.edu>
2018-03-30 20:12:54 +0900

    progress of BugInfestation src code without javadoc(yet)

--- src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
+++ src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
@@ -1,10 +1,38 @@
 package edu.handong.csee.java.lab06;
 
-public class BugInfestation {
+import java.util.Scanner;// import Scanner utility to get input from keyboard
 
+public class BugInfestation {// declare BugInfestation class
+
+	double growth_rate = 0.95, one_bug_volume = 0.002, house_volume = 0;
+	int countweeks = 0 ,population = 0;
+	
+	public void set(double houseVolume, int startPopulation) {
+		double house_volume = houseVolume;
+		int population = startPopulation;
+		bugvol();
+	}
+	
+	public void bugvol(){
+		double total_bug_volume = population * one_bug_volume;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int startPopulation; // declare left operator
+		double houseVolume; // declare right operator
+		BugInfestation bug = new BugInfestation(); // to use methods declared in Calculator class
+		
+		Scanner keyboard = new Scanner(System.in); // to get user input
+		
+		System.out.print("Enter house volume: "); // tells the user to input house volume
+		houseVolume = keyboard.nextDouble(); // get the user input as houseVolume
+		
+		System.out.print("Enter Starting Roach number: "); // tells the user to input roach number
+		startPopulation = keyboard.nextInt(); // get the user input of roaches as startPopulation
+		
+		bug.set(houseVolume,startPopulation); // set values of house_volume and population from user input
+		
 	}
 
 }
